64ca9b71048b10f9d957d051f3a23948e5b1d88d
Manoj Gautam <nix1947@users.noreply.github.com>
2024-02-13 13:41:34 +0545

    Updated

--- ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
+++ ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
@@ -5,17 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -38,6 +43,9 @@ import androidx.compose.ui.unit.sp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -78,47 +86,50 @@ fun ArtSpaceApp() {
             .padding(all = 20.dp)
     ) {
 
-        Column(
-            modifier = Modifier.height(400.dp)
-        ) {
+        Box(modifier=Modifier.padding(20.dp).background(color=Color.LightGray, shape = RoundedCornerShape(5.dp))) {
+            Column(
+                modifier = Modifier.height(400.dp).padding(20.dp).shadow(elevation = 12.dp)
+            ) {
 
-            when (currentImage) {
+                when (currentImage) {
 
-                0 -> Image(
-                    painter = painterResource(id = R.drawable.art_1), contentDescription = null,
-                    modifier = Modifier,
-                    contentScale = ContentScale.Crop
+                    0 -> Image(
+                        painter = painterResource(id = R.drawable.art_1), contentDescription = null,
+                        modifier = Modifier,
+                        contentScale = ContentScale.Crop
 
-                )
+                    )
 
-                1 -> Image(
-                    painter = painterResource(id = R.drawable.art_2), contentDescription = null,
-                    modifier = Modifier,
-                    contentScale = ContentScale.Crop
+                    1 -> Image(
+                        painter = painterResource(id = R.drawable.art_2), contentDescription = null,
+                        modifier = Modifier,
+                        contentScale = ContentScale.Crop
 
-                )
+                    )
 
-                2 -> Image(
-                    painter = painterResource(id = R.drawable.art_3), contentDescription = null,
-                    modifier = Modifier,
-                    contentScale = ContentScale.Crop
+                    2 -> Image(
+                        painter = painterResource(id = R.drawable.art_3), contentDescription = null,
+                        modifier = Modifier,
+                        contentScale = ContentScale.Crop
 
-                )
+                    )
+                }
+
+
             }
-
-
         }
 
         Spacer(modifier = Modifier.height(20.dp))
 
         // Title artist Year
         Row(
-            modifier = Modifier,
-            horizontalArrangement = Arrangement.Center
+            modifier = Modifier.background(color=Color.LightGray, shape = RoundedCornerShape(16.dp)).fillMaxWidth().height(80.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
 
         ) {
             Column(
-                modifier = Modifier,
+                modifier = Modifier
             ) {
                 Text(
                     text = titles[currentTitleIndex].first.uppercase(),
@@ -127,8 +138,6 @@ fun ArtSpaceApp() {
                     fontWeight = FontWeight.Bold,
                     modifier = Modifier.align(Alignment.CenterHorizontally)
                 )
-                Spacer(modifier = Modifier.height(20.dp))
-
                 Text(
                     text = titles[currentTitleIndex].second.uppercase(),
                     modifier = Modifier.align(Alignment.CenterHorizontally)
